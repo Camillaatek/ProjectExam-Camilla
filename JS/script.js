e02c3fbcb885325b55bbc13815d5309f42ab1ff8
Camillaatek <camillaatek@gmail.com>
2021-05-16 06:43:58 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -30,13 +30,47 @@
 
 
 
+    // const url = "https://camillaatek.no/wp-json/wp/v2/posts?_embed=true";
+    // const blog = document.querySelector(".blog")
+    
+    // const postList =(posts) => {
+    //     console.log(posts);
+    //     blog.innerHTML = "";
+    //     let postDiv = ``
+    //     for(post of posts){
+    //         console.log(post)
+    //         let images = post._embedded["wp:featuredmedia"]
+    //         console.log(images)
+    //         for (image of post._embedded["wp:featuredmedia"]){
+    //         postDiv = `
+    //        <div>
+    //         <a href="details.html?id=${post.id}"><h1>${post.title.rendered}</h1></a>
+    //         <a href="details.html?id=${post.id}"><img src="${image.source_url}" alt="hei"</a>
+    //         </div>
+    //         `;
+    //         blog.innerHTML += postDiv
+    //         }
+    
+    //     }
+    // }
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => postList(data))
+    //     .catch(error => blog.innerHTML = "Something went wrong");
+
+
+
+
+
+
+
+
     const url = "https://camillaatek.no/wp-json/wp/v2/posts?_embed=true";
     const blog = document.querySelector(".blog")
-    
-    const postList =(posts) => {
-        console.log(posts);
+
+    const postList = (posts) => {
+        console.log(posts)
         blog.innerHTML = "";
-        let postDiv = ``
         for(post of posts){
             console.log(post)
             let images = post._embedded["wp:featuredmedia"]
@@ -54,6 +88,6 @@
         }
     }
     fetch(url)
-        .then(response => response.json())
-        .then(data => postList(data))
-        .catch(error => blog.innerHTML = "Something went wrong");
+     .then(response => response.json())
+    .then(data => postList(data))
+    .catch(error => blog.innerHTML = "Something went wrong");
