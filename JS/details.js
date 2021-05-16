4a2938c09e4caa69a25fa73d62d4c26297feab4e
Camillaatek <camillaatek@gmail.com>
2021-05-16 06:43:52 +0200

    Update details.js

--- JS/details.js
+++ JS/details.js
@@ -1,8 +1,43 @@
-const getUrl = document.location.search
+// const getUrl = document.location.search
+// console.log (getUrl)
+
+// const para = new URLSearchParams (getUrl);
+// const id = para.get("id")
+// console.log(id)
+
+// const postList = `https://camillaatek.no/wp-json/wp/v2/posts/${id}?_embed=true`;
+// const posts = document.querySelector(".blog");
+
+    
+//     const postList =(posts) => {
+//         console.log(posts)
+//         blog.innerHTML = "";
+        
+            
+//            media._embedded["wp:featuredmedia"]
+//             for (images of blog){
+//                 console.log(images);
+//             postDiv = `
+//             <div>
+//             <h2>${details.title.rendered}</h2>
+//             <p>${post.content.rendered}</p>
+//             </div>
+//             `;
+//             blog.innerHTML += postDiv;
+//             }
+    
+//         }
+    
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => postList(data))
+//         .catch(error => blog.innerHTML = "Something went wrong");
+
+const getUrl = document.location.search;
 console.log (getUrl)
 
-const para = new URLSearchParams (getUrl);
-const id = para.get("id")
+const ny = new URLSearchParams (getUrl);
+const id = ny.get("id");
 console.log(id)
 
 const postList = `https://camillaatek.no/wp-json/wp/v2/posts/${id}?_embed=true`;
