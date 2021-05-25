0ae8ec370277d9cf7ef0cd6d8dd1b405d9cd99bf
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2021-05-25 15:37:04 +0200

    nesten

--- carousel.js
+++ carousel.js
@@ -1,10 +1,33 @@
-const api = "https://camillaatek.no/wp-json/wp/v2/posts?_embed=true";
-const carouselSlide = document.querySelector(".carouselSlide");
-const carouselContainer = document.querySelectorAll(".carouselContainer");
+const api = "https://camillaatek.no/wp-json/wp/v2/posts?_embed=true&per_page=100&per_page=100";
+const posts = document.querySelector(".blog");
+const carouselContainer = document.querySelector(".carouselContainer");
 
 
 
+const universe = (blogs) => {
+    console.log(blogs);
+    
+    let postDiv = ``
+    for (blog of blogs) {
+        
+        let images = blog._embedded["wp:featuredmedia"]
+        console.log(images);
+        for (image of blog._embedded["wp:featuredmedia"]){
+        postDiv = `  
+        <div class ="slides">
+        <a href="HTML/details.html?id=${blog.id}"><img src="${image.source_url}" alt="bilde"></a>
+        <a href="HTML/details.html?id=${blog.id}"><h2>${blog.title.rendered}</h2></a>
+       </div>
+       `;
+       posts.innerHTML += postDiv;
+       }
+   }
+}  
 
+fetch(api)
+    .then(response => response.json())
+    .then(data => universe(data))
+    .catch(error => console.error(error))
 
 
 
@@ -15,47 +38,35 @@ const nextButton = document.querySelector('#nextButton');
 
 
 let counter = 0;
-const size = carouselSlide.clientWidth;
+const size = carouselContainer.clientWidth;
 console.log(size)
 
 posts.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 
-
 nextButton.addEventListener('click',()=>{
+    if(counter >= 4){
+        counter= -1
+        posts.style.transform = 'translateX(0px)';
+    }
     posts.style.transition = "transform 0.4s ease-in-out";
     counter++;
-    posts.style.transition = 'translateX(' + (-size * counter) + 'px)';
-})
-nextButton.addEventListener('click',()=>{
+    posts.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    console.log(counter)
+});
+prevButton.addEventListener('click',()=>{
+    if(counter <= 0){
+        counter= 3
+        posts.style.transform = 'translateX(0px)';
+        console.log(counter)
+    }
     posts.style.transition = "transform 0.4s ease-in-out";
     counter--;
-    posts.style.transition = 'translateX(' + (-size * counter) + 'px)';
+    posts.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    console.log(counter)
+
 })
 
 
-const universe = (blogs) => {
-     console.log(blogs);
-     
-     let postDiv = ``
-     for (blog of blogs) {
-         
-         let images = blog._embedded["wp:featuredmedia"]
-         console.log(images);
-         for (image of blog._embedded["wp:featuredmedia"]){
-         postDiv = `  
-         <div class ="slides">
-         <a href="HTML/details.html?id=${blog.id}"><img src="${image.source_url}" alt="bilde"></a>
-         <a href="HTML/details.html?id=${blog.id}"><h2>${blog.title.rendered}</h2></a>
-        </div>
-        `;
-        posts.innerHTML += postDiv;
-        }
-    }
-}  
- 
-fetch(api)
-     .then(response => response.json())
-     .then(data => universe(data))
-     .catch(error => console.error(error))
+
 
