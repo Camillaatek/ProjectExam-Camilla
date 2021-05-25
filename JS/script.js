ae0cd63b1b4de154e4a9d865557349f01fc208a3
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2021-05-25 20:42:03 +0200

    kage

--- JS/script.js
+++ JS/script.js
@@ -64,8 +64,10 @@
 
 
 
-    const api = "https://camillaatek.no/wp-json/wp/v2/posts?_embed=true";
+    const api = "https://camillaatek.no/wp-json/wp/v2/posts?_embed=true&per_page=10";
+    const loadingButton = "https://camillaatek.no/wp-json/wp/v2/posts?_embed=true&per_page=2&offset=10";
     const posts = document.querySelector(".blog");
+    const load = document.querySelector(".loadingButton")
     
    
     const universe = (blogs) => {
@@ -79,8 +81,8 @@
              for (image of blog._embedded["wp:featuredmedia"]){
              postDiv = `  
              <div>
-             <a href="HTML/details.html?id=${blog.id}"><img src="${image.source_url}" alt="bilde"></a>
-             <a href="HTML/details.html?id=${blog.id}"><h2>${blog.title.rendered}</h2></a>
+             <a href="details.html?id=${blog.id}"><img src="${image.source_url}" alt="bilde"></a>
+             <a href="details.html?id=${blog.id}"><h2>${blog.title.rendered}</h2></a>
             </div>
             `;
             posts.innerHTML += postDiv;
@@ -94,6 +96,12 @@
          .catch(error => console.error(error))
     
 
-    
+load.addEventListener("click", ()=> {
+    fetch(loadingButton)
+    .then(response => response.json())
+    .then(data => universe(data))
+    .catch(error => console.error(error))
+    load.style.display="none"
+})
 
 
